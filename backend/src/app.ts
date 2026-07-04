ca99c3bf405e96589ac0b1661616281a5f759cb9
Carlos Antunes <carlosantunes.dev@gmail.com>
2026-07-04 20:03:32 -0300

    feat(user): adicionado a rota de usuários no app.ts principal

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,13 @@
 import express from 'express';
+import userRouter from './routes/user.route.js';
 
 const app = express();
-const admin = express();
-app.use('/admin', admin);
+app.use(express.json());
+app.use('/user', userRouter);
 
 app.listen(8000, (error) => {
   if (error) {
     throw error;
   }
   console.log('Servidor rodando na porta 8000');
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.get('/user/:id', (req, res) => {
-  res.send(`user ${req.params.id}`);
 });
