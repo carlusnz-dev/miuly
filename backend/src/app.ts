beb4ff0afdb8a3b2770d6e58d232f331c6a796ee
Carlos Antunes <carlosantunes.dev@gmail.com>
2026-07-04 20:16:23 -0300

    chore(backend): adicionado winston para melhor depuração durante o desenvolvimento

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import userRouter from './routes/user.route.js';
+import Logger from './lib/logger.js'; // classe Logger para log
 
 const app = express();
-app.use(express.json());
-app.use('/user', userRouter);
+app.use(express.json()); // formato JSON nas requisições
+app.use('/user', userRouter); // router para User
 
 app.listen(8000, (error) => {
   if (error) {
     throw error;
   }
-  console.log('Servidor rodando na porta 8000');
+  Logger.info('Servidor no ar e rodando em http://localhost:8000');
 });
