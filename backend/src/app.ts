c1a381139df366e53742c7d0376e68ef89f2045f
Carlos Antunes <carlosantunes.dev@gmail.com>
2026-07-14 20:26:13 -0300

    feat(router): nova rota adicionada sobre o model Finances no app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import userRouter from './routes/user.route.js';
 import Logger from './lib/logger.js'; // classe Logger para log
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import { financesRouter } from './routes/finances.router.js';
 
 const app = express();
 app.use(express.json()); // formato JSON nas requisições
@@ -10,6 +11,7 @@ app.use(cookieParser()); // cookie-parser para leitura dos cookies
 
 app.use('/user', userRouter); // router para User
 app.use('/auth', authRouter); // router para autenticação
+app.use('/finances', financesRouter);
 
 app.listen(8000, (error) => {
   if (error) {
---
+++ backend/src/routes/finances.router.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import {
+  createFinanceController,
+  findAllFinancesByUserIdController,
+} from '../controllers/finances.controller.js';
+import { authMiddleware } from '../middleware/auth.middleware.js';
+
+const financesRouter = express.Router();
+
+// rotas das finanças
+financesRouter.get('/all', authMiddleware, async (req, res) => {
+  findAllFinancesByUserIdController(req, res);
+});
+
+financesRouter.post('/create', authMiddleware, async (req, res) => {
+  createFinanceController(req, res);
+});
+
+export { financesRouter };
