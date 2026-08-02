e9404091612d8748ecf4b845b7f39b1d135c6159
Carlos Antunes <carlosantunes.dev@gmail.com>
2026-08-02 04:01:17 -0300

    feat(front): configurado app principal com os `provider`s da lib TaigaUI e cliente Http com interceptador.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,16 @@
+import { provideTaiga } from '@taiga-ui/core';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { credentialsInterceptor } from './core/interceptor/credentials-interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideHttpClient()],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([credentialsInterceptor])),
+    provideTaiga(),
+  ],
 };
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { TuiRoot } from '@taiga-ui/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Auth } from './core/auth/auth';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TuiRoot],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('frontend');
+
+  private readonly auth = inject(Auth);
+
+  constructor() {
+    this.auth.me().subscribe({
+      next: (res) => console.log('OK', res.data),
+      error: (err) => console.log('ERROR', err.status, err.error),
+    });
+  }
 }
